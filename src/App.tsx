5e27b12d2d33d451be99cc549e30e904ac73e148
jmasangcay <jmasangcay@sjcoe.net>
2024-04-10 10:24:50 -0700

    Added Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Routes, Route } from "react-router";
+import Dashboard from './pages/dashboard';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+        <Routes>
+          <Route path="/" element={<Dashboard />}/>
+        </Routes>
+    </>
+  )
+
 }
 
 export default App;
