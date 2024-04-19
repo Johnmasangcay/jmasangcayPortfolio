2a81489a77040c2d88645bc8898027e47a5bbe95
johnmasangcay <johnneilmasangcay28@yahoo.com>
2024-04-19 01:51:59 -0700

    Updated added skills and project section(Need to add the projects)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Routes, Route } from "react-router";
-import Home from './pages/home';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHtml5, faCss3, faJs, faReact, faAngular, faBootstrap } from '@fortawesome/free-brands-svg-icons';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import NavigationBar from './component/navigationBar';
+import sqlIcon from './img/SQL.jpg';
+import typescriptLogo from './img/typescript.jpg';
+import nextLogo from './img/nextjs.png';
+import cSharp from './img/c.jpg';
+import netCore from './img/netCore.png';
+import visualStudio from './img/vStudio.png';
+import vsCode from './img/vscode.png';
 
 const App = () => {
   return (
@@ -31,36 +39,128 @@ const App = () => {
           <hr />
         </div>
       </div>
-      {/* <div className="card flex-row flex-wrap">
-        <div className="card-header border-0">
-          <img src="//placehold.it/200" alt="" />
-        </div>
-        <div className="card-block px-2">
-          <h4 className="card-title">Title</h4>
-          <p className="card-text">Description</p>
-          <a href="#" className="btn btn-primary">BUTTON</a>
-        </div>
-        <div className="w-100"></div>
-      </div> */}
       <div className="container-fluid">
         <div className="row">
-          <div className="col-md-1"></div>
-          <div className="col-md-4">
-            <div className="card-header border-0">
-              <img src="//placehold.it/600" alt="" />
-            </div>
+          <div className="col-md-12 aboutMeDescription">
+            <p>Self-driven, ambitious, eager to learn and passionate are the characteristics that I have why you are reading this. I don't take things easily even if it's something that I've been doing
+              and I like getting things done.</p>
+            <p>I have the drive and have what it takes to be part of a progressing team and become a tool for the success of others and myself.</p>
           </div>
-          <div className="col-md-6">
-            <div className="card border-0">
-              <div className="card-body">
-                <div className="card-text">
-                  <p className="lineSpace"><br /><br /><span className="tab">Self-driven, ambitious, eager to learn and passionate</span> are the characteristics that I have why you are reading this. I don't take things easily even if it's something that I've been doing
-                    and I like getting things done.</p>
-                  <p className="lineSpace"><span className="tab">I have the drive and have what it takes to be part of</span> a progressing team and become a tool for the success of others and myself.</p>
-                </div>
-              </div>
-            </div>
+        </div>
+      </div>
+
+      {/* <!--- Skills Section --> */}
+      <div className="container-fluid d-flex justify-content-center" id="skills">
+        <div className="row welcome text-center">
+          <div className="col-12 p-3">
+            <h1><strong>Skills</strong></h1>
           </div>
+          <hr />
+        </div>
+      </div>
+      <div className="container-fluid">
+        <div className="row text-center">
+          <div className="col-xs-12 col-sm-6 col-md-3">
+            <FontAwesomeIcon className="fa-html5" icon={faHtml5} />
+            <h4>HTML5</h4>
+          </div>
+          <div className="col-xs-12 col-sm-6 col-md-3">
+            <FontAwesomeIcon className="fa-css3-alt" icon={faCss3} />
+            <h4>CSS3</h4>
+          </div>
+          <div className="col-xs-12 col-sm-6 col-md-3">
+            <FontAwesomeIcon className="fa-js" icon={faJs} />
+            <h4>JavaScript</h4>
+          </div>
+          <div className="col-xs-12 col-sm-6 col-md-3">
+            <FontAwesomeIcon className="fa-react" icon={faReact} />
+            <h4>React JS</h4>
+          </div>
+          <div className="col-sx-12 col-sm-6 col-md-3">
+            <img className="icon" src={typescriptLogo} alt="ypescript" />
+            <h4>Typescript</h4>
+          </div>
+          <div className="col-sx-12 col-sm-6 col-md-3">
+            <img className="icon" src={nextLogo} alt="NextJs" />
+            <h4>Next JS</h4>
+          </div>
+          <div className="col-xs-12 col-sm-6 col-md-3">
+            <FontAwesomeIcon className="fa-html5" icon={faAngular} />
+            <h4>Angular</h4>
+          </div>
+          <div className="col-xs-12 col-sm-6 col-md-3">
+            <img className="icon" src={cSharp} alt="C#" />
+            <h4>C#</h4>
+          </div>
+          <div className="col-xs-12 col-sm-6 col-md-3">
+            <img className="icon" src={netCore} alt="netCore" />
+            <h4>.NET CORE</h4>
+          </div>
+          <div className="col-xs-12 col-sm-6 col-md-3">
+            <FontAwesomeIcon className="fa-bold" icon={faBootstrap} />
+            <h4>Bootstrap</h4>
+          </div>
+          <div className="col-sx-12 col-sm-6 col-md-3">
+            <img className="icon" src={sqlIcon} alt="Sql" />
+            <h4>SQL Server</h4>
+          </div>
+          <div className="col-sx-12 col-sm-6 col-md-3">
+            <img className="icon" src={visualStudio} alt="Visual Studio" />
+            <h4>Visual Studio</h4>
+          </div>
+          <div className="col-sx-12 col-sm-6 col-md-3">
+            <img className="icon" src={vsCode} alt="ASP.NET" />
+            <h4>VS Code</h4>
+          </div>
+          <div className="col-xs-12 col-sm-6 col-md-3">
+            <FontAwesomeIcon className="fa-spin" icon={faSpinner} />
+            <h4>Coming Soon...</h4>
+          </div>
+          <hr className='welcome my-4' />
+        </div>
+      </div>
+
+      {/* <!-- Projects --> */}
+      <div className="container-fluid d-flex justify-content-center" id="projects">
+        <div className="row welcome text-center">
+          <div className="col-12">
+            <h1>Projects</h1>
+          </div>
+          <hr />
+        </div>
+      </div>
+      <div className="text-center">
+        <button type="button" className="btn btn-secondary" data-toggle="modal" data-target=".bd-example-modal-lg">View Projects</button>
+        <hr />
+      </div>
+
+      {/* <!--- Fixed background --> */}
+      <figure>
+        <div className="fixed-wrap">
+          <div className="fixed">
+          </div>
+        </div>
+      </figure>
+      {/* <figure>
+        <div className="fixed-wrap">
+          <div className="fixed">
+          </div>
+        </div>
+      </figure> */}
+
+      {/* <!--- Contact --> */}
+      <div className="container-fluid d-flex justify-content-center" id="contact">
+        <div className="row welcome text-center">
+          <h1>Contact</h1>
+          <div className="col-12">
+          </div>
+          <hr />
+        </div>
+      </div>
+      <div className="text-center">
+        <div className="col-12">
+          <p>Phone: (209) 641-7702</p>
+          <p>Email: johnneilmasangcay28@yahoo.com</p>
         </div>
       </div>
     </>
